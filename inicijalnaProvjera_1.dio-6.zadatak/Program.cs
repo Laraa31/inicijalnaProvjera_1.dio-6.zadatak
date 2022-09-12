5b5f40b21b1ca3117075dc282458e5baf2330de6
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2022-09-12 12:30:44 +0200

    Add files via upload

---
+++ inicijalnaProvjera_1.dio-6.zadatak/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inicijalnaProvjera_1.dio_6.zadatak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int brClanova;
+            double suma=0;
+
+            Console.WriteLine("Unesi broj članova reda: ");
+            brClanova =Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i < brClanova; i++)
+            {
+                suma = suma+1 / (3 * i);
+            }
+
+            Console.WriteLine(suma);
+            Console.ReadKey();
+
+        }
+    }
+}
